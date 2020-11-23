bd07a4ffbb339a801351f877f3ce23658887c8ea
Erhan <erhan404@gmail.com>
2020-11-23 14:20:01 +0300

    java2

---
+++ src/ders01/05170000069.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ders01;
+import java.io.BufferedReader
+import java.util.Scanner
+import java.io.BufferedInputStream
+/**
+ *
+ * @author 2ELDM604062
+ */
+public class Ders01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Scanner fileIn= null;
+     try
+         FileIn=new Scanner
+         new File inputStream("word.txt")
+     catch (FileNotFoundException)
+         system.out.printIn("Bulunamadı")
+                 
+                 
+               
+     
+           
+    }
+    
+}
